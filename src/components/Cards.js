5c54dc9e8cbe1c4cce65d5bd7dd051684769b269
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-01-30 00:24:25 +0530

    items card created

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,41 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import Cardsdata from "./CardsData";
+import "./style.css";
 const Cards = () => {
-  return <div>cards</div>;
+  const [data, setData] = useState(Cardsdata);
+  return (
+    <div className="container mt-3 ">
+      <h2 className="text-center">Add cart Part</h2>
+      <div className="row d-flex justify-content-center align-items-center">
+        {data.map((elem, id) => {
+          return (
+            <Card
+              style={{ width: "22rem", border: "none" }}
+              className="mx-2 mt-4 card_style"
+            >
+              <Card.Img
+                variant="top"
+                src={elem.imgdata}
+                style={{ height: "16rem" }}
+                className="mt-3"
+              />
+              <Card.Body>
+                <Card.Title>{elem.rname}</Card.Title>
+                <Card.Text>price : ₹{elem.price}</Card.Text>
+                <div className="button_div d-flex justify-content-center ">
+                  <Button variant="primary" className="col-lg-12">
+                    Add To Cart
+                  </Button>
+                </div>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cards;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Badge from "@mui/material/Badge";
 import { NavLink } from "react-bootstrap";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const Header = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -18,7 +29,15 @@ const Header = () => {
               Home
             </NavLink>
           </Nav>
-          <Badge badgeContent={4} color="primary">
+          <Badge
+            badgeContent={4}
+            color="primary"
+            id="basic-button"
+            aria-controls={open ? "basic-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
             <i
               class="fa fa-shopping-cart text-light"
               style={{ fontSize: 25, cursor: "pointer" }}
@@ -26,6 +45,39 @@ const Header = () => {
             ></i>
           </Badge>
         </Container>
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          <div
+            className="card_details d-flex justify-content-center align-items-center"
+            style={{ width: "24rem", padding: 10, position: "relative" }}
+          >
+            <i
+              className="fas fa-close smallclose"
+              onClick={handleClose}
+              style={{
+                position: "absolute",
+                top: 2,
+                right: 20,
+                fontSize: 23,
+                cursor: "pointer",
+              }}
+            ></i>
+            <p style={{ fontSize: 23 }}>Your Cart Is Empty!!! </p>
+            <img
+              src="./cart.gif"
+              alt="cart-gif"
+              className="emptycart_img"
+              style={{ width: "5rem", padding: 10 }}
+            />
+          </div>
+        </Menu>
       </Navbar>
     </>
   );
