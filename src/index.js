f44df1f0340429ce2e0b3fa6e83d32c9b85db73c
shubham pathak <121822895+shubham13101996@users.noreply.github.com>
2023-01-30 02:45:48 +0530

    create store and provide to the all the component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import store from "./store";
+import { Provider } from "react-redux";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
 );
---
+++ src/redux/reducers/main.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import { cartReducer } from "./reducer";
+
+const rootReducer = combineReducers({
+  cartReducer,
+});
+export default rootReducer;
